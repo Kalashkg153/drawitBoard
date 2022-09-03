4b7b7d95753e96013865baa721880bacb0e334a2
Kalashkg153 <kalashkg310@gmail.com>
2022-09-03 23:25:42 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 app.use(express.static("public"));
-let port =  process.env.port || 3000;
+let port =  process.env.PORT || 3000;
 let server = app.listen(port , ()=>{
     console.log("Listening to port " + port)
 })
